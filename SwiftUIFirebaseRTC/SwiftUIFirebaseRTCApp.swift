33986038594430f5cd1b58d8465c5e7e871992bf
citixenken <kmuyesu@gmail.com>
2022-03-29 06:53:26 +0300

    first commit

---
+++ SwiftUIFirebaseRTC/SwiftUIFirebaseRTCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseRTCApp.swift
+//  SwiftUIFirebaseRTC
+//
+//  Created by Ken Muyesu on 29/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseRTCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
